50657f119b60283b0ac1eb43b358ac2ceb7b928f
vongkol <hengvongkol@gmail.com>
2018-09-14 20:59:47 +0700

    add member

--- resources/views/admins/members/index.blade.php
+++ resources/views/admins/members/index.blade.php
@@ -3,8 +3,20 @@
 <div class="row">
     <div class="col-sm-12">
         <h3 class="page-header">
-            <i class="fa fa-user"></i> Members 
+            <div class="row">
+                <div class="col-sm-5">
+                    <i class="fa fa-user"></i> Members 
+                </div>
+                <div class="col-sm-7">
+                    <form action="#">
+                        <input type="text" placeholder="search...">
+                        <button>Search</button>
+                    </form>
+                </div>
+            </div>
+            
         </h3>
+      
         @if(Session::has('sms'))
             <div class="alert alert-success" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -35,25 +47,30 @@
                     $pagex = 1;
                     $i = 25 * ($pagex - 1) + 1;
                 ?>
-                @foreach($packages as $r)
+                @foreach($members as $m)
                 <tr>
                     <td>{{$i++}}</td>
-                    <td>{{$r->name}}</td>
-                    <td>{{$r->price}} $</td>
-                    <td>{{$r->monthly_payout}} %</td>
-                    <td>{{$r->duration}}</td>
                     <td>
-                        <a href="{{url('anana-admin/package/delete?id='.$r->id.'&page='.@$_GET['page'])}}" class="btn btn-danger btn-xs" 
+                        <a href="#" class="text-primary">{{$m->full_name}}</a>    
+                    </td>
+                    <td>{{$m->username}}</td>
+                    <td>{{$m->email}}</td>
+                    <td>{{$m->phone}}</td>
+                    <td>{{$m->country}}</td>
+                    <td>{{$m->sponsor_id}}</td>
+                    <td>
+                        <a href="{{url('anana-admin/member/detail/'.$m->id)}}" 
+                                class="btn btn-success btn-xs" title="View"><i class="fa fa-eye"></i></a>&nbsp;&nbsp;
+                        <a href="{{url('anana-admin/member/delete?id='.$m->id.'&page='.@$_GET['page'])}}" class="btn btn-danger btn-xs" 
                             title="Delete" onclick="return confirm('You want to delete?')">
-                        <i class="fa fa-trash"></i></a>&nbsp;&nbsp;
-                        <a href="{{url('anana-admin/package/edit/'.$r->id)}}" 
-                            class="btn btn-success btn-xs" title="Edit"><i class="fa fa-pencil"></i></a>
+                        <i class="fa fa-trash"></i></a>
+                       
                     </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
-        {{$packages->links()}}
+        {{$members->links()}}
     </div>
 </div>
 @endsection
