3cb215256b3c24036956b6933809c8faf221bba4
vichey <sorvichey>
2018-09-14 21:37:42 +0700

    layout payemtn

--- resources/views/fronts/investment.blade.php
+++ resources/views/fronts/investment.blade.php
@@ -7,18 +7,20 @@
     </section>
 </div>
 <div class="container my-5">
-    <div class="pricing card-deck ">
+    <div class="pricing row">
     @foreach($packages as $p)
-        <div class="card card-pricing shadow text-center col-md-3">
-            <span class="h6 w-60 mx-auto px-4 py-1 rounded-bottom bg-warning text-white shadow-sm">{{$p->name}}</span>
-            <div class="bg-transparent card-header pt-4 border-0">
-                <h1 class="h1 font-weight-normal text-warning text-center mb-0" data-pricing-value="15">$<span class="price">{{$p->price}}</span><span class="h6 text-muted ml-2">/ {{$p->duration}} days</span></h1>
-            </div>
-            <div class="card-body pt-0">
-                <ul class="list-unstyled mb-4">
-                    <h5>Payout: {{$p->monthly_payout}}%</h5>
-                </ul>
-                <button type="button" class="btn btn-outline-warning btn-blog rounded-0 mb-3">Order now</button>
+        <div class="col-md-3 my-3 px-2">
+            <div class="card card-pricing shadow text-center">
+                <span class="h6 w-60 mx-auto px-4 py-2  rounded-bottom bg-warning text-white shadow-sm">{{$p->name}}</span>
+                <div class="bg-transparent card-header px-1 border-0">
+                    <h1 class="h1 font-weight-normal text-warning text-center mb-0" data-pricing-value="15">$<span class="price">{{$p->price}}</span><span class="h6 text-muted ml-2">/ {{$p->duration}} days</span></h1>
+                </div>
+                <div class="card-body pt-0">
+                    <ul class="list-unstyled mb-4">
+                        <h5>Payout: {{$p->monthly_payout}}%</h5>
+                    </ul>
+                    <button type="button" class="btn btn-outline-warning btn-blog rounded-0 mb-3">Order now</button>
+                </div>
             </div>
         </div>
         @endforeach
